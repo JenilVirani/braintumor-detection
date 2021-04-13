4083966b7cec5e9f3cd9a38a3f548f55160b9998
JenilVirani <82446102+JenilVirani@users.noreply.github.com>
2021-04-13 19:19:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("https://braintumor-detection.herokuapp.com/")
 def demo():
     return "<html><body><h1>Hello World</h1></body></html>"
 
